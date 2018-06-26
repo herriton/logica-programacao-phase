dabd62915b33d86f979803550711863a58b3740e
Marcos Vinicius Dreher Oliveira <marcos.oliveira@mjv.com.br>
2018-06-26 10:32:22 -0300

    Atualizando projeto com inserção de código && ajustando inserção do terceiro slide

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var game = new Phaser.Game(1366, 768, Phaser.AUTO);
 
 game.state.add('Slide One', arrSlides[0]);
 game.state.add('Slide Two', arrSlides[1]);
+game.state.add('Slide Three', arrSlides[2]);
 game.state.start('Slide One');
 
 function addContent(p_arrContent) {
--- js/scenes/slide.js
+++ js/scenes/slide.js
@@ -50,131 +50,107 @@ var arrSlides = [
 
       game.load.spritesheet('interruptor','media/interruptor.png',28,36);
       game.load.spritesheet('light','media/light1.png',75,180);
-
     },
 
+    create : function(){
+      oRoomOFF = game.add.sprite(0,0,'room-light-off');
+      oRoomON = game.add.sprite(0,0,'room-light-on');
 
+      oRoomON.visible = false;
+
+      oRoomON.height = game.stage.height;
+      oRoomON.width = game.stage.width;
+
+      oRoomOFF.height = game.stage.height;
+      oRoomOFF.width = game.stage.width;
+
+      oInterruptor = game.add.button(150,400, 'interruptor', this.onClick);
+      oInterruptor.isOn = oInterruptor.frame == 2;
+      oInterruptor.frame = 1;
+
+      oLight = game.add.button(game.world.centerX, game.cache.getImage('light').height/2, 'light', this.onClick);
+      oLight.isOn = oLight.frame == 2;
+      oLight.anchor.set(.5);
+      oLight.frame = 1;
+    },
+
+    onClick : function(event){
+      switch(event.key){
+        case 'light':
+          if(oLight.isOn) {
+            oLight.isOn = !oLight.isOn;
+            oLight.frame = 1;
+
+            oInterruptor.isOn = !oInterruptor.isOn;
+            oInterruptor.frame = 1;
+
+            oRoomON.visible  = false;
+            oRoomOFF.visible = true;
+          } else {
+            oLight.isOn = !oLight.isOn;
+            oLight.frame = 2;
+
+            oInterruptor.isOn = false;
+            oInterruptor.frame = 1;
+
+            oRoomON.visible  = false;
+            oRoomOFF.visible = true;
+          }
+        break;
+        case 'interruptor':
+          if(oLight.isOn){
+            if(!oInterruptor.isOn){
+              oInterruptor.isOn = !oInterruptor.isOn;
+              oInterruptor.frame = 2;
+
+              oRoomON.visible  = true;
+              oRoomOFF.visible = false;
+            } else {
+              oInterruptor.isOn = !oInterruptor.isOn;
+              oInterruptor.frame = 1;
+
+              oRoomON.visible  = false;
+              oRoomOFF.visible = true;
+            }
+          } else {
+            oInterruptor.isOn = !oInterruptor.isOn;
+            oInterruptor.frame == 1 ? oInterruptor.frame = 2 : oInterruptor.frame = 1;
+          }
+        break;
+      }
+      //debugger;
+    },
+
+    update : function(){
+      var oKeys = { right: this.input.keyboard.justPressed(Phaser.Keyboard.RIGHT), left: this.input.keyboard.justPressed(Phaser.Keyboard.LEFT) }
+      if(oKeys.right || oKeys.left) {
+        delete oInterruptor;
+        delete oRoomOFF;
+        delete oRoomON;
+        delete oLight;
+      }
+      if(oKeys.right) this.state.start('Slide Three');
+      if(oKeys.left) this.state.start('Slide One');
+    }
+
+  },
+  {
+    preload:function(){
+      game.load.image('logic','media/logic.png');
+    },
 
     create : function(){
-          game.add.sprite(0,0,'room-light-off');
 
-          //var interrupt = game.add.sprite(150,400,'interruptor');
-
-          var interrupt = game.add.button(150,400, 'interruptor', interruptOnclick, 0);
-          var light;
-          var light2;
-
-      function up() {
-        console.log('button up', arguments);
-      }
-
-      function over() {
-        console.log('button over');
-      }
-
-      function out() {
-        console.log('button out');
-      }
-
-      function interruptOnclick () {
-
-          if(light.frame == 2 && interrupt.frame == 0){
-
-            interrupt.pendingDestroy = true;
-
-            game.add.sprite(0,0,'room-light-on');
-            var interruptOff = game.add.button(150,400, 'interruptor', interruptOffclick, 0);
-            interruptOff.frame = 1;
-
-            function up() {
-              console.log('button up', arguments);
-            }
-
-            function over() {
-              console.log('button over');
-            }
-
-            function out() {
-              console.log('button out');
-            }
-
-            function interruptOffclick () {
-                interrupt.pendingDestroy = true;
-
-                game.add.sprite(0,0,'room-light-off');
-                interruptOff = game.add.button(150,400, 'interruptor', interruptOnclick, 0);
-                interruptOff.frame = 0;
-
-            }
-
-              light2 = game.add.button(625,0, 'light', lightonclick, 0);
-              light2.frame = 3;
-
-              function up() {
-                console.log('button up', arguments);
-              }
-
-              function over() {
-                console.log('button over');
-              }
-
-              function out() {
-                console.log('button out');
-              }
-
-              function lightonclick () {
-
-              }
-
-
-          }else if(light.frame != 2 && interrupt.frame == 0){
-
-            interrupt.frame = 1;
-
-          }else if(light.frame != 2 && interrupt.frame == 1){
-
-            interrupt.frame = 0;
-          }else if(light.frame == 2 && interrupt.frame == 1){
-
-            interrupt.frame = 0;
-          }
-
-      }
-
-      light = game.add.button(625,0, 'light', lightoffclick, 0);
-
-      function up() {
-        console.log('button up', arguments);
-      }
-
-      function over() {
-        console.log('button over');
-      }
-
-      function out() {
-        console.log('button out');
-      }
-
-      function lightoffclick () {
-
-        switch (light.frame) {
-        case 0:
-          light.frame = 1;
-        break;
-        case 1:
-          light.frame = 2;
-        break;
-
-        }
-
-      }
+      var arrContent = [
+        { text: "Slide 3", posX: this.world.centerX, posY: 75, type:"title" },
+      ]
 
       addContent(arrContent);
     },
 
     update : function(){
-      //var oKeys = { Z: game.input.keyboard.justPressed(Phaser.Keyboard.Z) }
-      //if(oKeys.Z) game.state.start('Play');
+      var oKeys = { right: this.input.keyboard.justPressed(Phaser.Keyboard.RIGHT), left: this.input.keyboard.justPressed(Phaser.Keyboard.LEFT) }
+      if(oKeys.left) this.state.start('Slide Two');
     }
   }
 ]
